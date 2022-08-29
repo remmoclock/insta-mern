1ee3d1fcb65b1060785ca3ae6723b7550ac476e1
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-30 01:45:46 +0200

    hash password bcryptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => {
   res.send("hello home route");
@@ -19,20 +20,22 @@ router.post("/signup", (req, res) => {
           .status(422)
           .json({ error: "user already exists with that email" });
       }
-      const user = new User({
-        email,
-        password,
-        name,
-      });
-
-      user
-        .save()
-        .then(() => {
-          res.json({ message: "saved successfully" });
-        })
-        .catch((err) => {
-          console.log(err);
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          password: hashedpassword,
+          name,
         });
+
+        user
+          .save()
+          .then(() => {
+            res.json({ message: "saved successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     })
     .catch((err) => {
       console.log(err);
