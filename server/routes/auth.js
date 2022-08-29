5ced1db180301a18f80e5d32c2ba4710b5a8e05c
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-30 01:37:15 +0200

    save user in db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 router.get("/", (req, res) => {
   res.send("hello home route");
 });
 
 router.post("/signup", (req, res) => {
-  const { email, password, name } = req.body;
+  const { name, email, password } = req.body;
   if (!email || !password || !name) {
-    return res.json({ error: "please add all the fields" });
+    return res.status(422).json({ error: "please add all the fields" });
   }
-  return res.status(422).json({ message: "succes posted" });
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exists with that email" });
+      }
+      const user = new User({
+        email,
+        password,
+        name,
+      });
+
+      user
+        .save()
+        .then(() => {
+          res.json({ message: "saved successfully" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
