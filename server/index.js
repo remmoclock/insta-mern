76947fb678ed42a0cab5af76f2d295f715ef219e
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-29 02:08:55 +0200

    add .env data

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ require("dotenv").config();
 
 app.use(cors());
 
+const dbKey = process.env.USER_KEY
+
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
   res.send("hello world");
