993d155a6117c25da0b2d3bc61a7f08e5c7f4e6f
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-30 15:47:37 +0200

    send token jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.get("/", (req, res) => {
   res.send("hello home route");
@@ -59,13 +61,9 @@ router.post("/signin", (req, res) => {
       .compare(password, savedUser.password)
       .then((doMatch) => {
         if (doMatch) {
-          res.json({ message: "successfully signed in" });
-          //   const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
-          //   const { _id, name, email, followers, following, pic } = savedUser;
-          //   res.json({
-          //     token,
-          //     user: { _id, name, email, followers, following, pic },
-          //   });
+          //   res.json({ message: "successfully signed in" });
+          const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+          res.json({ token });
         } else {
           return res.status(422).json({ error: "Invalid Email or password" });
         }
