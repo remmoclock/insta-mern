43a595f7c13ea824bb570d9f6afa6d17cd4c712d
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-06 16:06:01 +0200

    create home screen

--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="card home-card">
+        <h5>Mario</h5>
+        <div className="card-image">
+          <img
+            className=""
+            src="https://mobilesyrup.com/wp-content/uploads/2021/04/super-mario-party-scaled.jpg"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title</h6>
+          <p>this is amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Mario</h5>
+        <div className="card-image">
+          <img
+            className=""
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK1PAys9jfEnH_P1x51l2O9cMpoJYTO0StjJToXoFslxARByADcj7AFYTp2QG8zirrySo&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title</h6>
+          <p>this is amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Mario</h5>
+        <div className="card-image">
+          <img
+            className=""
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyBM09MW7gO_i1jcIwKItCDpwNgxzMhwrFhvt9xjW9invW2brIFQiiEK8AcQ1EJXc3v7Y&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title</h6>
+          <p>this is amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
