5cdbed53e36caa785d5b3f4eeb04bac1eff6e7ce
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-10 23:36:19 +0200

    sign up post data

--- client/src/Components/screens/Signup.js
+++ client/src/Components/screens/Signup.js
@@ -1,14 +1,82 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
 function Signup() {
+  const history = useHistory();
+  const [password, setPasword] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  const PostData = () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(//eslint-disable-line
+        email
+      )
+    ) {
+      M.toast({
+        html: "invalid email",
+        classes: "#c62828 red darken-3 rounded",
+      });
+      return;
+    }
+    fetch("/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        password,
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          M.toast({
+            html: data.error,
+            classes: "#c62828 red darken-3 rounded",
+          });
+        } else {
+          M.toast({
+            html: data.message,
+            classes: "#43a047 green darken-1 rounded",
+          });
+          history.push("/signin");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="mycard">
       <div className="card auth-card input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPasword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          onClick={() => PostData()}
+        >
           Sign Up
         </button>
         <h5>
