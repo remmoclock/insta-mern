994c490bedd0ecc0bb4d61d42e7719ccb4e5e868
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-11 01:29:01 +0200

    Uploading image to cloudinary

--- client/src/Components/screens/CreatePost.js
+++ client/src/Components/screens/CreatePost.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreatePost() {
+  // const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  const postDetails = () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "insta-clone");
+    data.append("cloud_name", "mido92");
+    fetch("https://api.cloudinary.com/v1_1/mido92/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUrl(data.secure_url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div
       className="card input-filed"
@@ -11,18 +35,31 @@ function CreatePost() {
         textAlign: "center",
       }}
     >
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="body" />
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
       <div className="file-field input-field">
         <div className="btn #64b5f6 blue darken-1">
           <span>Uplaod Image</span>
-          <input type="file" />
+          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
         </div>
         <div className="file-path-wrapper">
           <input className="file-path validate" type="text" />
         </div>
       </div>
-      <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+      <button
+        className="btn waves-effect waves-light #64b5f6 blue darken-1"
+        onClick={() => postDetails()}
+      >
         Submit post
       </button>
     </div>
--- server/routes/post.js
+++ server/routes/post.js
@@ -26,8 +26,8 @@ router.get("/mypost", requireLogin, (req, res) => {
 });
 
 router.post("/createpost", requireLogin, (req, res) => {
-  const { title, body } = req.body;
-  if (!title || !body) {
+  const { title, body, pic } = req.body;
+  if (!title || !body || !pic) {
     return res.status(422).json({ error: "Please add all the fields" });
   }
   console.log(req.user);
@@ -35,6 +35,7 @@ router.post("/createpost", requireLogin, (req, res) => {
   const post = new Post({
     title,
     body,
+    pic,
     postedBy: req.user,
   });
   post
