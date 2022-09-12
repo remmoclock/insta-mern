af21f208db2398838e8d696159ae8ede07520836
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-13 00:44:04 +0200

    add routing function

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
+import { useEffect, createContext, useReducer, useContext } from "react";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import NavBar from "../src/Components/NavBar";
 import Home from "../src/Components/screens/Home";
 import SignIn from "./Components/screens/SignIn";
 import Signup from "../src/Components/screens/Signup";
 import Profile from "../src/Components/screens/Profile";
 import CreatePost from "./Components/screens/CreatePost";
+import { reducer, initialState } from "./reducers/userReducer";
 
-function App() {
+const UserContext = createContext();
+
+const Routing = () => {
+  const history = useHistory();
   return (
-    <div className="App">
-      <BrowserRouter>
-        <NavBar />
+    <>
+      <Switch>
         <Route exact path="/">
           <Home />
         </Route>
@@ -27,9 +31,20 @@ function App() {
         <Route path="/create">
           <CreatePost />
         </Route>
-      </BrowserRouter>
-    </div>
+      </Switch>
+    </>
+  );
+};
+
+function App() {
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    // <UserContext.Provider value={{ state, dispatch }}>
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
+    </BrowserRouter>
+    // </UserContext.Provider>
   );
 }
-
 export default App;
