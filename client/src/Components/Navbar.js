067550868224ae4d56385e06b0014ea94ab8a420
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-13 18:50:00 +0200

    fix css nav

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
   return (
     <nav>
       <div className="nav-wrapper white">
-        <Link to={state ? "/" : "/signin"} className="brand-logo left">
+        <Link to={state ? "/" : "/signin"} className="brand-logo left" style={{marginLeft : "5px"}}>
           Instagram
         </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
