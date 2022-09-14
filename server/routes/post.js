5bfee6d378450c0dd8a6e59ec71c90a756849810
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-15 00:02:00 +0200

    Like and unlike routes

--- server/routes/post.js
+++ server/routes/post.js
@@ -49,4 +49,41 @@ router.post("/createpost", requireLogin, (req, res) => {
     });
 });
 
+router.put("/like", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { likes: req.user._id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.put("/unlike", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      //remove the like with $pull
+      $pull: { likes: req.user._id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
 module.exports = router;
