e0aa60f0af2a75d41226ab1206468c8979ed93f1
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-15 14:44:54 +0200

    Delete post

--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../App";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [data, setData] = useState();
@@ -103,13 +104,50 @@ function Home() {
       });
   };
 
+  const deletePost = (postid) => {
+    fetch(`/deletepost/${postid}`, {
+      method: "delete",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        const newData = data.filter((item) => {
+          return item._id !== result._id;
+        });
+        setData(newData);
+      });
+  };
+
   return (
     <div className="home">
       {data?.map((item) => {
         return (
           <div className="card home-card" key={item._id}>
-            <h5 style={{ display: "grid", justifyContent: "center" }}>
-              {item.postedBy.name}
+            <h5 style={{ padding: "5px" }}>
+              <Link
+                to={
+                  item.postedBy._id !== state._id
+                    ? "/profile/" + item.postedBy._id
+                    : "/profile"
+                }
+              >
+                {item.postedBy.name}
+              </Link>
+              {item.postedBy._id === state._id && (
+                <i
+                  className="material-icons"
+                  style={{
+                    float: "right",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => deletePost(item._id)}
+                >
+                  delete
+                </i>
+              )}
             </h5>
             <div className="card-image">
               <img src={item.photo} />
