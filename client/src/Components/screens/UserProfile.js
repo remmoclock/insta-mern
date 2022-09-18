f82d4e2884483ce951dc849e0a4df3536ec0cb21
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-09-19 00:13:56 +0200

    User profile add follow/unfollow

--- client/src/Components/screens/UserProfile.js
+++ client/src/Components/screens/UserProfile.js
@@ -8,6 +8,7 @@ function UserProfile() {
 
   const [mypics, setPics] = useState([]);
   const [userProfile, setProfile] = useState();
+  const [showfollow, setShowFollow] = useState(true);
 
   console.log("userid", userid);
 
@@ -24,6 +25,71 @@ function UserProfile() {
         setProfile(result);
       });
   }, []);
+
+  const followUser = () => {
+    fetch("/follow", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        followId: userid,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: "UPDATE",
+          payload: { following: data.following, followers: data.followers },
+        });
+        localStorage.setItem("user", JSON.stringify(data));
+        setProfile((prevState) => {
+          return {
+            ...prevState,
+            user: {
+              ...prevState.user,
+              followers: [...prevState.user.followers, data._id],
+            },
+          };
+        });
+        setShowFollow(false);
+      });
+  };
+  const unfollowUser = () => {
+    fetch("/unfollow", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        unfollowId: userid,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: "UPDATE",
+          payload: { following: data.following, followers: data.followers },
+        });
+        localStorage.setItem("user", JSON.stringify(data));
+
+        setProfile((prevState) => {
+          const newFollower = prevState.user.followers.filter(
+            (item) => item != data._id
+          );
+          return {
+            ...prevState,
+            user: {
+              ...prevState.user,
+              followers: newFollower,
+            },
+          };
+        });
+        setShowFollow(true);
+      });
+  };
 
   return (
     <>
@@ -58,30 +124,38 @@ function UserProfile() {
                 }}
               >
                 <h6>{userProfile.posts.length} posts</h6>
-                {/* <h6>{userProfile.user.followers.length} followers</h6> */}
-                {/* <h6>{userProfile.user.following.length} following</h6> */}
+                <h6>{userProfile.user.followers.length} followers</h6>
+                <h6>{userProfile.user.following.length} following</h6>
               </div>
-              {/* {showfollow ? (
-                <button
-                  style={{
-                    margin: "10px",
-                  }}
-                  className="btn waves-effect waves-light #64b5f6 blue darken-1"
-                  onClick={() => followUser()}
-                >
-                  Follow
-                </button>
+              {showfollow ? (
+                <>
+                  <i
+                    className="material-icons"
+                    onClick={() => followUser()}
+                    style={{
+                      cursor: "pointer",
+                      margin: "10px",
+                    }}
+                  >
+                    add_circle
+                  </i>
+                  <span>Follow</span>
+                </>
               ) : (
-                <button
-                  style={{
-                    margin: "10px",
-                  }}
-                  className="btn waves-effect waves-light #64b5f6 blue darken-1"
-                  onClick={() => unfollowUser()}
-                >
-                  UnFollow
-                </button>
-              )} */}
+                <>
+                  <i
+                    className="material-icons"
+                    onClick={() => unfollowUser()}
+                    style={{
+                      cursor: "pointer",
+                      margin: "10px",
+                    }}
+                  >
+                    check_box
+                  </i>
+                  <span>Unfollow</span>
+                </>
+              )}
             </div>
           </div>
 
