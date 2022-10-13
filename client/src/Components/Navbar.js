c8688237656c1eab154309c4077fc118d5e618dd
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-10-13 16:12:01 +0200

    search user

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,14 +1,30 @@
 import React, { useContext, useRef, useEffect, useState } from "react";
 import { UserContext } from "../App";
 import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
 function Navbar() {
   const { state, dispatch } = useContext(UserContext);
   const history = useHistory();
+  const searchModal = useRef(null);
+  const [search, setSearch] = useState("");
+  const [userDetails, setUserDetails] = useState([]);
+  useEffect(() => {
+    M.Modal.init(searchModal.current);
+  }, []);
 
   const renderList = () => {
     if (state) {
       return [
+        <li key="1">
+          <i
+            data-target="modal1"
+            className="large material-icons modal-trigger"
+            style={{ color: "black" }}
+          >
+            search
+          </i>
+        </li>,
         <li key="1">
           <Link to="/profile">Profile</Link>
         </li>,
@@ -44,6 +60,23 @@ function Navbar() {
     }
   };
 
+  const fetchUsers = (query) => {
+    setSearch(query);
+    fetch("/search-users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+      }),
+    })
+      .then((res) => res.json())
+      .then((results) => {
+        setUserDetails(results.user);
+      });
+  };
+
   return (
     <nav>
       <div className="nav-wrapper white">
@@ -57,6 +90,46 @@ function Navbar() {
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           {renderList()}
         </ul>
+      </div>
+      <div
+        id="modal1"
+        class="modal"
+        ref={searchModal}
+        style={{ color: "black" }}
+      >
+        <div className="modal-content">
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            onChange={(e) => fetchUsers(e.target.value)}
+          />
+          <ul className="collection">
+            {userDetails.map((item) => {
+              return (
+                <Link
+                  to={
+                    item._id !== state._id ? "/profile/" + item._id : "/profile"
+                  }
+                  onClick={() => {
+                    M.Modal.getInstance(searchModal.current).close();
+                    setSearch("");
+                  }}
+                >
+                  <li className="collection-item">{item.email}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="modal-footer">
+          <button
+            className="modal-close waves-effect waves-green btn-flat"
+            onClick={() => setSearch("")}
+          >
+            close
+          </button>
+        </div>
       </div>
     </nav>
   );
